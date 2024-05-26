9ebe5fb21846a98f8d09b1e5c51939f215a46b3e
Enzzzzz <contatoenzz@gmail.com>
2024-05-26 19:08:02 -0300

    first navbar

--- src/templates/navbars/navbar.jsx
+++ src/templates/navbars/navbar.jsx
@@ -1,20 +1,36 @@
 
-const Navbar = () => {
+function Navbar() {
   return (
-    <nav className='bg-gray-200 border-gray-200'>
-        <div className="max-w-screen-xl bg-gray-200 flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="/logo.png" alt="logo" className="size-14"/>
-            <span className="self-center text-3xl font-semibold whitespace-nowrap text-sky-800">Enzzzzz</span>
-            </a>
-            <div className="flex md:order-2">               
+    <div className="fixed top-0 w-full z-50 bg-gray-300 border-b border-primary">
+      <div className="bg-gray-300 flex flex-wrap items-center justify-between mx-[10%] p-3">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="/logo.png" alt="logo" className="size-12" />
+          <span className="self-center text-2xl text-gradient font-semibold whitespace-nowrap text-sky-800">
+            Enzzzzz
+          </span>
+        </a>
 
-            </div>
-            
+        <div className="w-[30%] cursor-pointer flex flex-how items-center justify-between px-5 bg-gray-200 hover:bg-gray-300 duration-300 rounded-lg h-10 neon-blue">
+          <h1 className="transform animate-pulse duration-200 text-lg font-bold text-secondary">
+            AVISO
+          </h1>
+          <p className="text-sm font-felix text-secondary">NOVO TEMPLATE DISPONÍVEL</p>
+
         </div>
-      
-    </nav>
-  )
+        <div className="flex flex-row space-x-10">
+          <h1 className="navbutton">
+            Contato
+          </h1>
+          <h1 className="navbutton">
+            Templates
+          </h1>
+          <h1 className="navbutton">
+            Projetos
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
